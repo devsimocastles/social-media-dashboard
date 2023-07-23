49b783e0caba189e136ddbcd358ed9f430715678
Simon Castillo <simocastles@gmail.com>
2023-07-23 08:46:50 -0300

    mobile version finished

--- src/components/StatsCard.jsx
+++ src/components/StatsCard.jsx
@@ -1,18 +1,34 @@
 import React from "react";
 import { chooseIcon } from "../App";
-import "../sass/main.css"
-
+import "../sass/main.css";
 
 const StatsCard = (props) => (
-    <div className="stat-card-container">
-        <div className={"color-bar "+props.socialIcon}></div>
-        <p>
-            <img src={chooseIcon(props.socialIcon)} alt="Social Media Icon" className="social-icon" />
-            {props.user}
-        </p>
-        <p className="followers-cantity">{props.followers}</p>
-        <p className="followers-title">{props.socialIcon != "yt" ? "FOLLOWERS" : "SUSCRIBERS"}</p>
-        <p className={props.positiveBalance ? "followers-status positive" : "followers-status negative"}><img src={chooseIcon(props.arrow)} alt="arrow icon"/>{props.newFollowers} Today</p>
+    <div className="stat-card">
+        <div className={"color-bar " + props.socialIcon}></div>
+        <div className="content">
+            <p className="username">
+                <img
+                    src={chooseIcon(props.socialIcon)}
+                    alt="Social Media Icon"
+                    className="social-icon"
+                />
+                {props.user}
+            </p>
+            <p className="followers-cantity">{props.followers}</p>
+            <p className="followers-title">
+                {props.socialIcon != "yt" ? "FOLLOWERS" : "SUSCRIBERS"}
+            </p>
+            <p
+                className={
+                    props.positiveBalance
+                        ? "followers-status positive"
+                        : "followers-status negative"
+                }
+            >
+                <img src={chooseIcon(props.arrow)} alt="arrow icon" />
+                {props.newFollowers} Today
+            </p>
+        </div>
     </div>
 );
 
